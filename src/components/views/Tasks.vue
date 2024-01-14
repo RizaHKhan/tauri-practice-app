b5d2b11fe198f789b45ef4c5f261522ec724566b
khanr <khanriza@gmail.com>
2024-01-14 17:55:41 -0500

    Adding interface for tasks

--- src/components/views/Tasks.vue
+++ src/components/views/Tasks.vue
@@ -5,23 +5,34 @@
                 <p>
                     {{ task.description }}
                 </p>
+                <template #footer>
+                    <Button warn @click="removeTask">Remove</Button>
+                </template>
             </Card>
         </div>
     </View>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { Ref, ref } from 'vue'
 import { invoke } from '@tauri-apps/api'
 import { onMounted } from 'vue'
 import Card from 'atoms/Card.vue'
 import View from 'atoms/View.vue'
+import Button from 'atoms/Button.vue'
 
-const tasks = ref([])
+interface Task {
+    title: string
+    description: string
+}
+
+const tasks: Ref<Task[] | []> = ref([])
 
 onMounted(async () => {
     tasks.value = await invoke('get_tasks')
 })
+
+const removeTask = () => {}
 </script>
 
 <style lang="scss" scoped>
